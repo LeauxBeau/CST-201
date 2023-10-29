4ecd30b1dcd51bd8afe124932602acdd0b40e444
LeauxBeau <leauxbeau@gmail.com>
2023-10-28 20:15:15 -0500

    Update to Project Zero
    Updated bubble sort method and added binary search method.
    
    Tested and works.

--- Project Zero/ProjectZero/src/app/main.java
+++ Project Zero/ProjectZero/src/app/main.java
@@ -3,92 +3,117 @@ package app;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class main {
-	public static void main(String[] args) {
-		  String[] words = new String[10000];
-	        int size = 0;
+    public static void main(String[] args) {
+        String[] wordArray = new String[10000];
+        int wordCount = 0;
 
-	        // Read strings from the file and store them in the array
-	        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\austi\\Desktop\\Repositories (GitHub)\\CST-201\\Project Zero\\ProjectZero\\Text"))) {
-	            String line;
-	            while ((line = br.readLine()) != null && size < 10000) {
-	                words[size++] = line;
-	            }
-	        } catch (IOException e) {
-	            System.err.println("Error reading the file: " + e.getMessage());
-	            System.exit(1);
-	        }
+        // Step 1: Read from a text file and store words in the array
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\austi\\Desktop\\Repositories (GitHub)\\CST-201\\Project Zero\\ProjectZero\\Text"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    if (wordCount < 10000) {
+                        wordArray[wordCount] = word;
+                        wordCount++;
+                    }
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }	
+        System.out.println("Unsorted Array: ");
+        printArray(wordArray);
 
-	        // Print the unsorted array
-	        System.out.println("Unsorted Array:");
-	        for (int i = 0; i < size; i++) {
-	            System.out.println(words[i]);
-	        }
+        // Step 2: Check if the array is full and print it
+        if (wordCount >= 10000) {
+            System.out.println("Unsorted Array:");
+            printArray(wordArray);
+        }
 
-	        // Sort the array using bubble sort
-	        bubbleSort(words, size);
+        // Step 3: Implement a selective sort method to sort the array
+        selectiveSort(wordArray, wordCount);
 
-	        // Print the sorted array
-	        System.out.println("\nSorted Array:");
-	        for (int i = 0; i < size; i++) {
-	            System.out.println(words[i]);
-	        }
+        // Step 4: Implement a binary search method to search for words
+        binarySearch(wordArray, wordCount);
+    }
 
-	        // Search for words using binary search
-	        Scanner scanner = new Scanner(System.in);
-	        while (true) {
-	            System.out.print("\nEnter a word to search for (or type 'exit' to quit): ");
-	            String target = scanner.nextLine();
+    // Print the array
+    public static void printArray(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
 
-	            if (target.equals("exit")) {
-	                break;
-	            }
+    // Sort the array alphabetically using selective sort
+    public static void selectiveSort(String[] arr, int size) {
+        for (int i = 0; i < size - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < size; j++) {
+                if (arr[j].compareTo(arr[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+            String temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        System.out.println("Sorted Array:");
+        printArray(arr);
+    }
 
-	            int index = binarySearch(words, size, target);
+    // Implement binary search
+    public static void binarySearch(String[] arr, int size) {
+    	   Scanner scanner = new Scanner(System.in);
+    	    String searchWord;
 
-	            if (index != -1) {
-	                System.out.println("Word found at index: " + index);
-	            } else {
-	                System.out.println("Word not in the list.");
-	            }
-	        }
-	    }
+    	    while (true) {
+    	        System.out.print("Enter a word to search (or type 'exit' to quit): ");
+    	        searchWord = scanner.nextLine();
 
-	    // Bubble sort to sort the array
-	    private static void bubbleSort(String[] arr, int size) {
-	        for (int i = 0; i < size - 1; i++) {
-	            for (int j = 0; j < size - i - 1; j++) {
-	                if (arr[j].compareTo(arr[j + 1]) > 0) {
-	                    String temp = arr[j];
-	                    arr[j] = arr[j + 1];
-	                    arr[j + 1] = temp;
-	                }
-	            }
-	        }
-	    }
+    	        if (searchWord.equalsIgnoreCase("exit")) {
+    	            System.out.println("Exiting search.");
+    	            break;
+    	        }
 
-	    // Binary search to search for a word in the sorted array
-	    private static int binarySearch(String[] arr, int size, String target) {
-	        int left = 0;
-	        int right = size - 1;
+    	        int result = binarySearchWord(arr, size, searchWord);
+    	        if (result != -1) {
+    	            System.out.println("Word '" + searchWord + "' found at index " + result);
+    	        } else {
+    	            System.out.println("Word '" + searchWord + "' not found in the list.");
+    	        }
+    	    }
 
-	        while (left <= right) {
-	            int mid = left + (right - left) / 2;
+    	    scanner.close();
+    	}
 
-	            int cmp = target.compareTo(arr[mid]);
+    	// Binary search method for searching a word in the sorted array
+    	public static int binarySearchWord(String[] arr, int size, String searchWord) {
+    	    int left = 0;
+    	    int right = size - 1;
 
-	            if (cmp == 0) {
-	                return mid; // Found
-	            } else if (cmp < 0) {
-	                right = mid - 1;
-	            } else {
-	                left = mid + 1;
-	            }
-	        }
+    	    while (left <= right) {
+    	        int mid = left + (right - left) / 2;
+    	        int compareResult = searchWord.compareTo(arr[mid]);
 
-	        return -1; // Not found
-	    }
-	}
+    	        if (compareResult == 0) {
+    	            return mid; // Word found at index mid
+    	        }
+
+    	        if (compareResult < 0) {
+    	            right = mid - 1;
+    	        } else {
+    	            left = mid + 1;
+    	        }
+    	    }
+
+    	    return -1; // Word not found
+    	}	
+}
